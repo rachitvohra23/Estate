1bfdd438fb7da072b95603891a0e2720fd6e9d27
Rachit Vohra <rachitvohra23@gmail.com>
2024-06-11 21:58:36 +0530

    new repo

---
+++ client/src/firebase.js
@@ -0,0 +1,34 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-6d771.firebaseapp.com",
+  projectId: "mern-estate-6d771",
+  storageBucket: "mern-estate-6d771.appspot.com",
+  messagingSenderId: "909780774583",
+  appId: "1:909780774583:web:1b944a0fb58fb2e971e732",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
